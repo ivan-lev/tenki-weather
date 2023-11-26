2b7533b46740c65c3f0a6b8ba73eee762f6951a0
Ivan Lev <fanatos@mail.ru>
2023-11-26 17:06:40 +0500

    style: code refactoring

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import styles from './Main.module.scss';
-import weatherConditions from '../../vendor/weather_conditions.json';
 
 const Main = ({ weatherData }) => {
-  //console.log(weatherConditions);
-
-  //const weatherIcon = `../../src/images/icons/day/${weatherData?.current?.condition?.code}.png`;
-  const weatherIcon = `../../src/images/icons/day/113.png`;
+  const weatherIcon = `https:${weatherData?.current?.condition?.icon}`;
 
   return (
     <section className={styles.main}>
       <h2 className={styles.main__header}>Погода в твоей местности:</h2>
       {weatherData && (
         <p className={styles.main__text}>
-          На улице:&nbsp;{weatherData?.current.condition.text.toLowerCase()}
+          На улице:&nbsp;{weatherData?.current.condition.text.toLowerCase()}&nbsp;
           <img className={styles['main__weather-icon']} src={weatherIcon} alt="Weather icon"></img>
         </p>
       )}
