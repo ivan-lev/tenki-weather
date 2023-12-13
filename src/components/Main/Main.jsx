9175df68469606cced98549e63b5c572505620b2
Ivan Lev <fanatos@mail.ru>
2023-12-13 11:58:40 +0500

    feat: add place name and fix styles

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import styles from './Main.module.scss';
+import './Main.scss';
 
-const Main = ({ weatherData }) => {
+const Main = ({ placeName, weatherData }) => {
   const weatherIcon = `https:${weatherData?.current?.condition?.icon}`;
 
   return (
-    <section className={styles.main}>
-      <h2 className={styles.main__header}>Погода в твоей местности:</h2>
+    <section className="main">
+      <p>{placeName}</p>
+      <h2 className="main__header">Погода в твоей местности:</h2>
       {weatherData && (
-        <p className={styles.main__text}>
+        <p className="main__text">
           На улице:&nbsp;{weatherData?.current.condition.text.toLowerCase()}&nbsp;
-          <img className={styles['main__weather-icon']} src={weatherIcon} alt="Weather icon"></img>
+          <img className="main__weather-icon" src={weatherIcon} alt="Weather icon"></img>
         </p>
       )}
-      <p className={styles.main__text}>Температура: {weatherData?.current?.temp_c}&deg;C</p>
-      <p className={styles.main__text}>Ощущается как: {weatherData?.current?.feelslike_c}&deg;C</p>
-      <p className={styles.main__text}>Влажность: {weatherData?.current?.humidity}&#37;</p>
+      <p className="main__text">Температура: {weatherData?.current?.temp_c}&deg;C</p>
+      <p className="main__text">Ощущается как: {weatherData?.current?.feelslike_c}&deg;C</p>
+      <p className="main__text">Влажность: {weatherData?.current?.humidity}&#37;</p>
     </section>
   );
 };
